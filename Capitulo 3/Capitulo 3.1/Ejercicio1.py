629fe69c51879c1e349aaf4b89b2e391e8b04e97
Jorgeei <101459642+Jorgeei@users.noreply.github.com>
2022-03-18 22:33:38 -0500

    Actualizacion

--- Capitulo 3/Capitulo 3.1/Ejercicio1.py
+++
@@ -1,7 +0,0 @@
-r=int(input("digite el radio: "))
-pi=float(input("digite el valor de pi: "))
-
-a=(pi*(r**2))
-
-print("el area del circulo es: ")
-print(a)
--- Capitulo 3/Capitulo 3.1/Ejercicio6.py
+++
@@ -1,9 +0,0 @@
-v=int(input("valor de la compra: "))
-d=12/100
-d1=v*d
-vr=v-d1
-
-print("el valor del descuento es: ")
-print(d)
-print(vr)
-
