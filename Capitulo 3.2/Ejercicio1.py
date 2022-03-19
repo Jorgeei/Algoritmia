039bd8e9d1910287f1027e9a9eb45a54567066a3
Jorgeei <101459642+Jorgeei@users.noreply.github.com>
2022-03-18 22:44:35 -0500

    Actualizacion

--- Capitulo 3.2/Ejercicio1.py
+++
@@ -1,7 +0,0 @@
-Nombre=input("digite el nombre: ")
-Apellidos=input("digite los apellido: ")
-
-n_a=(Apellidos+','+Nombre)
-
-print("Nombre y Apellido: ",n_a)
-print(n_a)
--- Capitulo 3.2/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-ruta=("//1.1.1.1/eoi/python")
-
-ruta=ruta[2:]
-equipo=ruta[:7]
-ruta =ruta[-11:]
-e_r=(f"equipo={equipo}; ruta={ruta}")
-print(e_r)
