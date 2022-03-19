466755f34c683fd0c7b29a15af39673a819d3b0a
Jorgeei <101459642+Jorgeei@users.noreply.github.com>
2022-03-18 22:44:12 -0500

    Actualizacion

--- Capitulo 3.1/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-r=int(input("digite el radio: "))
-pi=float(input("digite el valor de pi: "))
-
-v=(4/3)*pi*(r**3)
-
-print("el volumen de la esfera es: ")
-print(v)
--- Capitulo 3.1/Ejercicio3.py
+++
@@ -1,7 +0,0 @@
-b=int(input("digite la base: "))
-a=int(input("digite la altura: "))
-
-h=(4*5)/(2)
-
-print("el area del triangulo es: ")
-print(h)
