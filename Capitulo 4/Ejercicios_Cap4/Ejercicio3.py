2babb390aa22434451098922fff6835752751bee
Jorgeei <101459642+Jorgeei@users.noreply.github.com>
2022-03-26 18:13:14 -0500

    Actualización1

---
+++ Capitulo 4/Ejercicios_Cap4/Ejercicio3.py
@@ -0,0 +1,4 @@
+v_numerico1=int(input('digite el primer velor: '))
+v_numerico2=int(input('digite el segundo valor: '))
+operando=input('digite el operando: ')
+
---
+++ Capitulo 4/Ejercicios_Cap4/Ejercicio_color.py
@@ -0,0 +1,10 @@
+color='#AF549B'
+
+if color=='#FF0000':
+    print("🔴")
+elif color=='00FF00':
+    print("🟢")
+elif color=='#0000FF':
+    print("🔵")
+else:
+    print("No es ningun color")
